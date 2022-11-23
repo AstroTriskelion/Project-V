547aff3972eae20049a69c37ff595cc6be1c63cd
AstroTriskelion <82666345+AstroTriskelion@users.noreply.github.com>
2022-11-23 23:02:03 +0000

    new changes

---
+++ Assets/_Scripts/_Scene1_Blue_Script/WaitForGrab2.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+public class WaitForGrab2 : AStateBehaviour
+{
+    public bool grabTheLantern = false;
+    public TriggerAudioEvent triggerAudioEventScript;
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    public override bool InitializeState()
+    {
+
+        return true;
+    }
+
+    public override void OnStateStart()
+    {
+        Debug.Log("Grab it");
+    }
+
+    public override void OnStateUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GrabedTheLantern();
+        }
+    }
+
+    public override void OnStateEnd()
+    {
+
+    }
+
+    public override int StateTransitionCondition()
+    {
+        if (grabTheLantern == true)
+        {
+            SceneManager.LoadScene("IndoorField_Juliette", LoadSceneMode.Single);
+        }
+        return (int)ELanternIntroductionStates.Invalid;
+    }
+    public void GrabedTheLantern()
+    {
+        //grabTheLantern = true;
+        triggerAudioEventScript.RequestAudio();
+        triggerAudioEventScript.OnAudioFinished += OnAudioCompleted;
+        Debug.Log("You grabbed it");
+        return;
+    }
+
+    public void OnAudioCompleted()
+    {
+        grabTheLantern = true;
+    }
+}
+
