be108d7e76e2a2c808c2c5b7eeb74cd6ba5b82cc
UE-NelsonCosta <91540209+UE-NelsonCosta@users.noreply.github.com>
2023-01-12 19:14:45 +0000

    Rotation fix Lantern

--- Assets/BezierController.cs
+++ Assets/BezierController.cs
@@ -8,6 +8,7 @@ public class BezierController : MonoBehaviour
     public Transform lanternPosition;
     private float tiltAngle = 0f;
     public Transform resetPosition;
+    public Rigidbody laternRigidBody;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,9 @@ public class BezierController : MonoBehaviour
     public void ActivateBezier()
     {
         script.enabled = true;
-        Vector3 newRotation = new Vector3(resetPosition.transform.eulerAngles.x, resetPosition.transform.eulerAngles.y, resetPosition.transform.eulerAngles.z);
-        lanternPosition.transform.eulerAngles = newRotation;
-        //lanternPosition.rotation = Quaternion.Euler(resetPosition.localRotation.eulerAngles.x, resetPosition.localRotation.eulerAngles.y, resetPosition.localRotation.eulerAngles.z); //Quaternion.Slerp(resetPosition.rotation, resetPosition.rotation, resetPosition.rotation);
+        laternRigidBody.freezeRotation = true;
+        lanternPosition.transform.position = resetPosition.position;
+        lanternPosition.transform.rotation = resetPosition.rotation;
     }
 
     public void DisactivateBezier()
